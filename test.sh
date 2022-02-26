491a37b5e70a119688ea8ddfd0df644939e88f91
tinaxd <31384595+tinaxd@users.noreply.github.com>
2022-02-26 21:10:40 +0900

    added test

--- test.sh
+++ test.sh
@@ -78,5 +78,6 @@ assert 5 "int add(int a, int b) {return a+b;} int sub(int c, int d) {return c-d;
 assert 5 "int add(int a, int b) {return a+b;} int sub(int a, int b) {return a-b;} int main() {return add(2, sub(5, 2));}"
 assert 55 "int fibo(int x) { if(x==0) return 0; if(x==1) return 1; return fibo(x-1)+fibo(x-2);} int main(){return fibo(10);}"
 assert 3 "int main() {int x; int y; x=3; y=&x; return *y;}"
+assert 3 "int main() {int x; int *y; y = &x; *y = 3; return x;}"
 
 echo OK
